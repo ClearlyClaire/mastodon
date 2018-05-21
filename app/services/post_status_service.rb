08e78b88759de4341384579213d2897c9c7c5ad3
Thibaut Girka <thib@sitedethib.com>
2018-05-21 20:21:52 +0200

    Backend changes to allow handling local-only toots not based on emoji

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -168,6 +168,7 @@ class PostStatusService < BaseService
       visibility: @visibility,
       language: language_from_option(@options[:language]) || @account.user&.setting_default_language&.presence || LanguageDetector.instance.detect(@text, @account),
       application: @options[:application],
+      local_only: options[:local_only],
     }.compact
   end
 
