65a60a88498373c527c1b08b9245fc873b2bb53e
kibigo! <go@kibi.family>
2022-05-08 21:52:06 -0700

    Fix tests which expected public visibility
    We changed the default to unlisted, and it broke these.

--- spec/controllers/api/v1/timelines/public_controller_spec.rb
+++ spec/controllers/api/v1/timelines/public_controller_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe Api::V1::Timelines::PublicController do
   render_views
@@ -11,49 +11,49 @@ describe Api::V1::Timelines::PublicController do
     allow(controller).to receive(:doorkeeper_token) { token }
   end
 
-  context 'with a user context' do
+  context "with a user context" do
     let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id) }
 
-    describe 'GET #show' do
+    describe "GET #show" do
       before do
-        PostStatusService.new.call(user.account, text: 'New status from user for federated public timeline.')
+        PostStatusService.new.call(user.account, text: "New status from user for federated public timeline.", visibility: :public)
       end
 
-      it 'returns http success' do
+      it "returns http success" do
         get :show
 
         expect(response).to have_http_status(200)
-        expect(response.headers['Link'].links.size).to eq(2)
+        expect(response.headers["Link"].links.size).to eq(2)
       end
     end
 
-    describe 'GET #show with local only' do
+    describe "GET #show with local only" do
       before do
-        PostStatusService.new.call(user.account, text: 'New status from user for local public timeline.')
+        PostStatusService.new.call(user.account, text: "New status from user for local public timeline.", visibility: :public)
       end
 
-      it 'returns http success' do
-        get :show, params: { local: true }
+      it "returns http success" do
+        get :show, params: {local: true}
 
         expect(response).to have_http_status(200)
-        expect(response.headers['Link'].links.size).to eq(2)
+        expect(response.headers["Link"].links.size).to eq(2)
       end
     end
   end
 
-  context 'without a user context' do
+  context "without a user context" do
     let(:token) { Fabricate(:accessible_access_token, resource_owner_id: nil) }
 
     before do
       Setting.timeline_preview = true
     end
 
-    describe 'GET #show' do
-      it 'returns http success' do
+    describe "GET #show" do
+      it "returns http success" do
         get :show
 
         expect(response).to have_http_status(200)
-        expect(response.headers['Link']).to be_nil
+        expect(response.headers["Link"]).to be_nil
       end
     end
   end
--- spec/controllers/api/v1/timelines/tag_controller_spec.rb
+++ spec/controllers/api/v1/timelines/tag_controller_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe Api::V1::Timelines::TagController do
   render_views
@@ -11,30 +11,30 @@ describe Api::V1::Timelines::TagController do
     allow(controller).to receive(:doorkeeper_token) { token }
   end
 
-  context 'with a user context' do
+  context "with a user context" do
     let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id) }
 
-    describe 'GET #show' do
+    describe "GET #show" do
       before do
-        PostStatusService.new.call(user.account, text: 'It is a #test')
+        PostStatusService.new.call(user.account, text: "It is a #test", visibility: :public)
       end
 
-      it 'returns http success' do
-        get :show, params: { id: 'test' }
+      it "returns http success" do
+        get :show, params: {id: "test"}
         expect(response).to have_http_status(200)
-        expect(response.headers['Link'].links.size).to eq(2)
+        expect(response.headers["Link"].links.size).to eq(2)
       end
     end
   end
 
-  context 'without a user context' do
+  context "without a user context" do
     let(:token) { Fabricate(:accessible_access_token, resource_owner_id: nil) }
 
-    describe 'GET #show' do
-      it 'returns http success' do
-        get :show, params: { id: 'test' }
+    describe "GET #show" do
+      it "returns http success" do
+        get :show, params: {id: "test"}
         expect(response).to have_http_status(200)
-        expect(response.headers['Link']).to be_nil
+        expect(response.headers["Link"]).to be_nil
       end
     end
   end
