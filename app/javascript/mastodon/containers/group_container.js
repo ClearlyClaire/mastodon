ebadd25ac52234a62bc4a91bb4b4139be9a83cf0
Claire <claire.github-309c@sitedethib.com>
2022-09-22 14:50:01 +0200

    Add group search results to WebUI

---
+++ app/javascript/mastodon/containers/group_container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Group from '../components/group';
+
+const mapStateToProps = (state, { id }) => ({
+  group: state.getIn(['groups', id]),
+});
+
+export default connect(mapStateToProps)(Group);
